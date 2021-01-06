c02e44193156e0254b560be9bc6a67efad5ea521
Katie Otto <33523737+kateotto@users.noreply.github.com>
2021-01-06 09:58:53 +0100

    setup project

---
+++ src/conf/api.js
@@ -0,0 +1,2 @@
+export const api =
+  "http://www.randomnumberapi.com/api/v1.0/random?min=1&max=6&";
--- src/main.js
+++ src/main.js
@@ -2,8 +2,19 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { BootstrapVue } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import axios from "axios";
+import { VueAxios } from "vue-axios";
+
+import { api } from "./conf/api";
 
 Vue.config.productionTip = false;
+
+Vue.prototype.$apiUrl = api;
+
+Vue.use(BootstrapVue, VueAxios, axios);
 
 new Vue({
   router,
